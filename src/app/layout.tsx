93544e7e75e9ce172f9ad22cbdd06bf555a336bb
Sabbir Hossen <Sabbir Hossen>
2025-03-21 02:00:20 +0600

    added title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Technical Assessment | Grit System",
+  title: "Calender | Grit System",
   description: "A calendar to manage rate availability",
 };
 
